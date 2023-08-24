67082439faea9f682e919d5892c5cb866e6e9b45
Májai Heni <majai.henrietta@student.ms.sapientia.ro>
2023-08-24 14:35:14 +0300

    AU-17 Forgot Password Page

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { Head, useForm } from '@inertiajs/vue3';
 import AuthenticationCard from '@/Components/AuthenticationCard.vue';
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -21,14 +20,13 @@ const submit = () => {
 </script>
 
 <template>
+<div class="forgot-pw-page w-screen h-screen"> 
     <Head title="Forgot Password" />
-
+        <Navbar />
     <AuthenticationCard>
-        <template #logo>
             <AuthenticationCardLogo />
-        </template>
-
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="bg-white h-[33rem] rounded-r-lg p-5 w-[20rem] shadow-2xl">
+        <div class=" mt-32 mb-4 text-sm text-gray-600">
             Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
         </div>
 
@@ -57,5 +55,39 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
+        </div>
     </AuthenticationCard>
+    </div>
 </template>
+
+<script>
+import Navbar from '@/Components/Navbar.vue'
+export default {
+    components: {
+        Navbar,
+    }
+}
+</script>
+
+<style scoped>
+.forgot-pw-page, .forgot-pw-page::before {
+    background-image: url(./images/bg.png);
+    background-size: cover;
+    width: 100%;
+}
+
+.forgot-pw-page {
+    background: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+}
+
+.forgot-pw-page::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    opacity: 60%;
+    z-index: -1;
+}
+</style>
