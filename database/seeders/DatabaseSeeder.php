d8f421e0505a6a94706f7b251551c46c116c376f
Gál István-Zsolt <gal.istvan.zsolt@student.ms.sapientia.ro>
2023-08-24 09:55:37 +0300

    AU-2 Teacher/Student register

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RolesSeeder;
+//use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            RolesSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_admin = Role::create(['name' => 'admin']);
+        $role_teacher = Role::create(['name' => 'teacher']);
+        $role_student = Role::create(['name' => 'student']);
+
+        $permissions_admin = [];
+        $permissions_teacher = [];
+        $role_admin->syncPermissions($permissions_admin);
+        $role_teacher->syncPermissions($permissions_teacher);
+    }
+}
