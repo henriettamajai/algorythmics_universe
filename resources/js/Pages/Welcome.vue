3e894f62410501645bfda6cdc83cd7c38928d6a3
Májai Heni <majai.henrietta@student.ms.sapientia.ro>
2023-08-24 10:04:38 +0300

    AU-15 Register page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,13 +3,12 @@
     <Navbar />
 
     <div class="text-white p-12 m-32 rounded-lg ">
-    <div class="bg-opacity-50 bg-gray-400 backdrop-blur rounded-lg shadow-xl p-8 border-2 border-black border-opacity-50">
+    <div class="bg-opacity-50 bg-gray-400 backdrop-blur rounded- shadow-xl p-8 border-2 border-black border-opacity-50">
         <h1 class="text-5xl text-center  m-6">Welcome to Algorythmics Universe: Your Gateway to Programming Mastery!</h1>
         <p class="text-2xl tracking-wide m-12 ">
           Are you ready to unlock the world of programming? Look no further! Algorythmics Universe is your one-stop destination for mastering the basics of programming and testing your skills through interactive quizzes. Whether you're a curious beginner or someone looking to solidify your programming foundation, we've got you covered.
         </p>
-        <a href="/register" class="border-2 border-black border-opacity-50 rounded-full bg-purple-900 px-8 py-2 text-white text-xl hover:bg-purple-700 m-8 shadow-xl ">Get Started</a>
-
+        <a href="/register" class=" px-4 py-2 bg-purple-800 border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-700  focus:ring-offset-2 transition ease-in-out duration-150">Get Started</a>
       </div>
     </div>
   </div>
