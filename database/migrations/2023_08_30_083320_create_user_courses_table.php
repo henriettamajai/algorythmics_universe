b6551ecdb0571dc20b6d454f602f3b0a2ddc4322
Gál István-Zsolt <gal.istvan.zsolt@student.ms.sapientia.ro>
2023-08-30 14:15:57 +0300

    AU-19 Fixes 2
    Switched out the integer to foreignId

--- database/migrations/2023_08_30_083320_create_user_courses_table.php
+++ database/migrations/2023_08_30_083320_create_user_courses_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_courses', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('course_id');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('course_id')->constrained();
             $table->timestamps();
         });
     }
