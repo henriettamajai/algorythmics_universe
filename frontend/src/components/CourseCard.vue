18732c49c62ebef7f0243dd4c9f748b8e7dbc86d
Májai Henrietta - Zita <majai.henrietta@student.ms.sapientia.ro>
2024-03-19 20:19:49 +0200

    Added badges to the cards and added the component into the code

--- frontend/src/components/CourseCard.vue
+++ frontend/src/components/CourseCard.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class="border-2 border-purple-500 max-w-sm bg-white/10 shadow-lg rounded-lg overflow-hidden m-4 ">
-      <img :src="course.image" alt="Course Image" class="w-full h-32 object-cover" />
-      <div class="p-4">
-        <h3 class="text-xl font-semibold text-white uppercase tracking-widest">{{ course.title }}</h3>
-        <p class="text-white mt-2">{{ course.description }}</p>
-        <a :href="`/game${course.id}`">
-          <button class="mt-4 bg-purple-500 text-white px-4 py-2 rounded-lg hover:bg-purple-600 uppercase tracking-widest">
-            Start now
-          </button>
-        </a>
+  <div class="border-2 border-purple-500 max-w-sm bg-white/10 shadow-lg rounded-lg overflow-hidden m-4 ">
+    <img :src="course.image" alt="Course Image" class="w-full h-32 object-cover" />
+    <div class="p-4">
+      <h3 class="text-xl font-semibold text-white uppercase tracking-widest">{{ course.title }}</h3>
+      <div class="mt-2">
+        <span v-for="(badge, index) in course.badges" :key="index" class="inline-block bg-purple-200 text-purple-800 p-2 rounded-full text-xs font-bold tracking-widest mr-2">{{ badge }}</span>
       </div>
+      <p class="text-white mt-2">{{ course.description }}</p>
+      
+      <a :href="`/game${course.id}`">
+        <button class="mt-4 bg-purple-500 text-white px-4 py-2 rounded-lg hover:bg-purple-600 uppercase tracking-widest">
+          Start now
+        </button>
+      </a>
     </div>
-  </template>
-  
+  </div>
+</template>
 
 <script>
-  export default {
-    props: {
-      course: {
-        type: Object,
-        required: true
-      }
+export default {
+  props: {
+    course: {
+      type: Object,
+      required: true
     }
   }
-  </script>
-  
+}
+</script>
