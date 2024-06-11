d3b44ac6a31f20f5df66c0689052704be48e9c6a
Májai Henrietta - Zita <majai.henrietta@student.ms.sapientia.ro>
2024-06-12 00:08:30 +0300

    Added the items to the variables game

---
+++ frontend/src/game1/Collectible.js
@@ -0,0 +1,22 @@
+export class Collectible {
+  constructor(type, value, position, imageSrc) {
+    this.type = type;
+    this.value = value;
+    this.position = position;
+    this.collected = false;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+
+  draw(ctx) {
+    if (!this.collected) {
+      ctx.drawImage(this.image, this.position.x, this.position.y, 40, 40);
+      ctx.fillStyle = 'white';
+      ctx.textAlign = 'center';
+      const centerX = this.position.x + 20; 
+      const centerY = this.position.y + 22;
+      ctx.fillText(this.value, centerX, centerY);
+      
+    }
+  }
+}
--- frontend/src/game1/Game1.vue
+++ frontend/src/game1/Game1.vue
@@ -9,7 +9,7 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import { onMounted } from 'vue';
 import { Vector2 } from './Vector2';
 import { resources } from './Resource';
@@ -17,73 +17,112 @@ import { Sprite } from './Sprite';
 import { GameLoop } from './GameLoop';
 import { Input, LEFT, RIGHT, UP, DOWN } from './Input.js';
 import Navigation from '@/components/Navigation.vue';
+import { Collectible } from './Collectible'; 
 
-onMounted(() => {
-  startGame(); 
-});
+export default {
+  setup() {
+    onMounted(() => {
+      startGame(); 
+    });
 
-const startGame = () => {
-  const canvas = document.getElementById("game-canvas");
-  const ctx = canvas.getContext("2d");
+    const startGame = () => {
+      const canvas = document.getElementById("game-canvas");
+      const ctx = canvas.getContext("2d");
 
-  const skySprite = new Sprite({
-    resource: resources.images.sky,
-    frameSize: new Vector2(1280, 720)
-  });
+      const skySprite = new Sprite({
+        resource: resources.images.sky,
+        frameSize: new Vector2(1280, 720)
+      });
 
-  const groundSprite = new Sprite({
-    resource: resources.images.ground,
-    frameSize: new Vector2(1280, 720)
-  });
+      const groundSprite = new Sprite({
+        resource: resources.images.ground,
+        frameSize: new Vector2(1280, 720)
+      });
 
-  const character = new Sprite({
-    resource: resources.images.character,
-    frameSize: new Vector2(64, 64),
-    hFrames: 4,
-    vFrames: 4,
-    frame: 1
-  });
+      const character = new Sprite({
+        resource: resources.images.character,
+        frameSize: new Vector2(64, 64),
+        hFrames: 4,
+        vFrames: 4,
+        frame: 1
+      });
 
-  const characterPos = new Vector2((canvas.width - 64) / 2, (canvas.width - 64) / 2);
-  const input = new Input();
+      const characterPos = new Vector2((canvas.width - 64) / 2, (canvas.width - 64) / 2);
+      const input = new Input();
 
-  const update = () => {
-    if (input.direction === DOWN) {
-      characterPos.y += 1;
+     //initializing items
+      const collectibles = [
+      new Collectible('int', 42, new Vector2(100, 100), 'sprites/circle.png'),
+      new Collectible('string', 'moon', new Vector2(200, 200),'sprites/moon.png'),
+      new Collectible('boolean', 'true', new Vector2(150, 100), 'sprites/circle2.png'),
+      new Collectible('char', 'A', new Vector2(250, 200),'sprites/circle3.png'),
+      new Collectible('float', 12.5, new Vector2(250, 250),'sprites/rock.png'),
+      ];
+
+      const update = () => {
+        if (input.direction === DOWN) {
+          characterPos.y += 1;
+        }
+        if (input.direction === UP) {
+          characterPos.y -= 1;
+        }
+        if (input.direction === LEFT) {
+          characterPos.x -= 1;
+          character.frame = 14;
+        }
+        if (input.direction === RIGHT) {
+          characterPos.x += 1;
+          character.frame = 8;
+        }
+
+        collectibles.forEach(item => {
+    if (!item.collected &&
+        characterPos.x < item.position.x + 32 &&
+        characterPos.x + 64 > item.position.x &&
+        characterPos.y < item.position.y + 32 &&
+        characterPos.y + 64 > item.position.y) {
+      item.collected = true;
+      askQuestion(item);
     }
-    if (input.direction === UP) {
-      characterPos.y -= 1;
-    }
-    if (input.direction === LEFT) {
-      characterPos.x -= 1;
-      character.frame = 14;
-    }
-    if (input.direction === RIGHT) {
-      characterPos.x += 1;
-      character.frame = 8;
-    }
-  };
+        });
+      };
 
-  const draw = () => {
-    skySprite.drawImage(ctx, 0, 0);
+      const draw = () => {
+        skySprite.drawImage(ctx, 0, 0);
 
-    const groundWidth = 720;
-    const groundHeight = 720;
+        const groundWidth = 720;
+        const groundHeight = 720;
 
-    const x = (canvas.width - groundWidth) / 2;
-    const y = (canvas.height - groundHeight) / 2;
+        const x = (canvas.width - groundWidth) / 2;
+        const y = (canvas.height - groundHeight) / 2;
 
-    groundSprite.drawImage(ctx, x, y, groundWidth, groundHeight);
+        groundSprite.drawImage(ctx, x, y, groundWidth, groundHeight);
 
-    character.drawImage(ctx, characterPos.x, characterPos.y);
-  };
+        character.drawImage(ctx, characterPos.x, characterPos.y);
+        collectibles.forEach(item => item.draw(ctx));
+      };
 
-  const gameLoop = new GameLoop(update, draw);
-  gameLoop.start();
+      const gameLoop = new GameLoop(update, draw);
+      gameLoop.start();
+
+    };
+    return {
+      startGame
+    };
+  }
+};
+
+const askQuestion = (item) => {
+  const answer = prompt(`Mi a változó típusa ennek az értéknek: ${item.value}?`);
+  if (answer !== null && ((item.type === 'number' && answer.toLowerCase() === 'number') ||
+      (item.type === 'string' && answer.toLowerCase() === 'string'))) {
+    alert('Correct answer! Item collected!');
+  } else {
+    alert('Incorrect answer! Try again.');
+    item.collected = false;
+  }
 };
 </script>
-
-
 
 <style>
 .centered-container {
